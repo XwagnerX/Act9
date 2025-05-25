6d69a7e419fef216c074c01b5b450a81f434f71b
XwagnerX <wgaleix@gmail.com>
2025-05-25 20:47:38 +0200

    Configurar base de datos para usar variables de entorno y agregar archivos de configuracion de Vercel

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'agenda_viajes',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'agenda_viajes',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
